7323925cf59bbb3b85970e61c7c51cb6a189932a
Regitaamelia <100113433+Regitaamelia@users.noreply.github.com>
2023-04-09 20:51:59 +0700

    feature: login page (#3)

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import HomeService from "./pages/HomeService";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomeService />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
