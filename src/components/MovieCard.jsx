cc435164734a6927a059d9e86a8eb332f33a97fb
Arham Akhmad Fachrezy <124849258+rezzy88@users.noreply.github.com>
2023-04-13 05:56:35 +0700

    feature: details (#8)

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 const imageUrl = "https://image.tmdb.org/t/p/w500";
 
-function MovieCard({ movie }) {
+function MovieCard({ movie, showLink = true }) {
   console.log(movie.poster_path);
   return (
     <div className="movie-card text-center">
@@ -14,6 +15,11 @@ function MovieCard({ movie }) {
       </div>
       <div className="movie-title">{movie.title}</div>
       <div className="movie-date"> {movie.release_date}</div>
+      {showLink && (
+<Link to={`/movie/${movie.id}`} className="btn btn-primary">
+Details
+</Link>
+)}
     </div>
   );
 }
